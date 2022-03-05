6ed68aac67b61738930084e3709c9579f15f0bde
Agiruskiy <80781755+Agiruskiy@users.noreply.github.com>
2022-03-05 21:45:19 +0300

    minor updates

--- 8yearsbot.py
+++ 8yearsbot.py
@@ -8,14 +8,12 @@ def kremlin(update: Updater, context: CallbackContext):
   command_counter = randint(1, 5)
   if command_counter == 4:
       update.message.reply_text('Данное сообщение отправлено лицом поддерживающим вторжение в Украину #standwithukraine🇺🇦')
-  else:
-      pass
 
 def main():
 # ADD TOKEN
   my_bot = Updater(TOKEN)
-  my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('(([лЛгГ].[тТдД][aАуУ]?|[вВсС])\s*(2014|8|[вВ][оО][сС].?[мМ][ьЬъЪиИ]?)(ой)?|(8|[вВ]?[оО][сС].?[мМ][ьЬъЪиИ]?([оО][йЙ])?)\s*[лЛгГ].[тТдД][aАуУ]?)([^а-яa-z]|\Z)'), years))
-  my_bot.dispatcher.add_handler(MessageHandler(Filters.user(user_id = {773498806, 132431205}), kremlin))
+  my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('(([лЛгГ].[тТдД][aАуУ]?|[вВсС])\s*(2014|8|[вВ][оО][сС].?[мМ][ьЬъЪиИ]?)(ой)?|(8|[вВ]?[оО][сС].?[мМ][ьЬъЪиИ]?([оО][йЙ])?)\s*[лЛгГ].[тТдД][aАуУ]?)([^а-яa-z]|\Z)'), years), 0)
+  my_bot.dispatcher.add_handler(MessageHandler(Filters.user(user_id = {773498806, 132431205}), kremlin), 1)
   my_bot.start_polling()
   my_bot.idle()
 
