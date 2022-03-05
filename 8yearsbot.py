bace0970605bb811c9a2cbc56eef375d6c889524
Agiruskiy <80781755+Agiruskiy@users.noreply.github.com>
2022-03-05 22:03:24 +0300

    .env added

--- 8yearsbot.py
+++ 8yearsbot.py
@@ -1,5 +1,9 @@
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 from random import randint
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def years(update: Updater, context: CallbackContext):
   update.message.reply_sticker('CAACAgIAAxkBAAEECn1iIOkPGHzDqt2Zv8I1g-J5o--RjwACFBYAAma5CEmQib3xor7ODCME')
@@ -9,9 +13,10 @@ def kremlin(update: Updater, context: CallbackContext):
   if command_counter == 4:
       update.message.reply_text('Данное сообщение отправлено лицом поддерживающим вторжение в Украину #standwithukraine🇺🇦')
 
+
 def main():
 # ADD TOKEN
-  my_bot = Updater(TOKEN)
+  my_bot = Updater(getenv("TOKEN"))
   my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('(([лЛгГ].[тТдД][aАуУ]?|[вВсС])\s*(2014|8|[вВ][оО][сС].?[мМ][ьЬъЪиИ]?)(ой)?|(8|[вВ]?[оО][сС].?[мМ][ьЬъЪиИ]?([оО][йЙ])?)\s*[лЛгГ].[тТдД][aАуУ]?)([^а-яa-z]|\Z)'), years), 0)
   my_bot.dispatcher.add_handler(MessageHandler(Filters.user(user_id = {773498806, 132431205}), kremlin), 1)
   my_bot.start_polling()
