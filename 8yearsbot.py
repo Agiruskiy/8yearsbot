8b95aab061f41a7c84739b905cde7a4c8b887582
Agiruskiy <80781755+Agiruskiy@users.noreply.github.com>
2022-03-05 21:17:44 +0300

    kremlin func update

--- 8yearsbot.py
+++ 8yearsbot.py
@@ -4,13 +4,13 @@ def years(update: Updater, context: CallbackContext):
     update.message.reply_sticker('CAACAgIAAxkBAAEECn1iIOkPGHzDqt2Zv8I1g-J5o--RjwACFBYAAma5CEmQib3xor7ODCME')
 
 def kremlin(update: Updater, context: CallbackContext):
-    update.message.reply_sticker('CAACAgIAAxkBAAEECntiIOijLlvH3YF0f8z32KM3PkOSewACbBUAAmUj8UgWwTqsNu6FTyME')
+    update.message.reply_text('Данное сообщение отправлено лицом поддерживающим вторжение в Украину #standwithukraine🇺🇦')
 
 def main():
 # ADD TOKEN
     my_bot = Updater(TOKEN)
     my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('(([лЛгГ].[тТдД][aАуУ]?|[вВсС])\s*(2014|8|[вВ][оО][сС].?[мМ][ьЬъЪиИ]?)(ой)?|(8|[вВ]?[оО][сС].?[мМ][ьЬъЪиИ]?([оО][йЙ])?)\s*[лЛгГ].[тТдД][aАуУ]?)([^а-яa-z]|\Z)'), years))
-    my_bot.dispatcher.add_handler(MessageHandler(Filters.user(user_id = 773498806), kremlin))
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.user(user_id = {773498806, 132431205}), kremlin))
     my_bot.start_polling()
     my_bot.idle()
 
